11f7092fe31f41ae85b09bc697eb76e50dafab67
Alex <alexandru.toma15@imperial.ac.uk>
2016-05-19 16:59:34 +0100

    Added file reading and outputing for assemble.c

--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,31 @@
+#include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_LINE_LENGTH 512
+
 int main(int argc, char **argv) {
+  // Check for number of arguments
+  if (argc != 3) {
+    fprintf(stderr, "The function needs 2 arguments!");
+    return EXIT_FAILURE;
+  }
+
+  FILE *input = fopen(argv[1], "r");
+  FILE *output;
+
+  // check file existance throw error if not found
+  if (!input) {
+    fprintf(stderr, "The file %s was not found", argv[1]);
+    fclose(input);
+    return EXIT_FAILURE;
+  }
+
+  output = fopen(argv[2], "wb");
+
+  //fwrite(&x, sizeof(int), 1, output);
+
+  fclose(input);
+  fclose(output);
+
   return EXIT_SUCCESS;
 }
