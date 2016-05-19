a73ba8cd79d5ea42dc31a86e80dd190e93b488b2
Andrei-Octavian Brabete <ab7515@texel10.doc.ic.ac.uk>
2016-05-19 16:44:57 +0100

    Attempt to read from binary file

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,15 @@
 #include <stdlib.h>
+#include<stdio.h>
 
 int main(int argc, char **argv) {
+  FILE *file = fopen(argv[0], "wb");
+  char *line;
+  if(file == NULL) {
+    err(1, "Enter valid file");
+  }
+  while (!feof(file)) {
+    fread(line, 1, 50, file);
+    /*process line(what instr, )*/
+  }
   return EXIT_SUCCESS;
 }
