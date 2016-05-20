ca9044a12a2249b398801e8e76fbc0decf850184
ab7515 <andrei-octavian.brabete15@imperial.ac.uk>
2016-05-20 17:03:49 +0100

    Emulator update-Binary file loader(not complete)

--- src/emulate.c
+++ src/emulate.c
@@ -1,15 +1,20 @@
 #include <stdlib.h>
 #include<stdio.h>
 
+#define MEM_SIZE_BYTES 
+
+typedef struct proc_state {
+  int NEG;
+  int ZER;
+  int CRY;
+  int OVF;
+  int regs[17];
+  int memory[][32];
+} proc_state_t;
+
 int main(int argc, char **argv) {
-  FILE *file = fopen(argv[0], "wb");
-  char *line;
-  if(file == NULL) {
-    err(1, "Enter valid file");
-  }
-  while (!feof(file)) {
-    fread(line, 1, 50, file);
-    /*process line(what instr, )*/
-  }
+  FILE *file = fopen(argv[0], "rb");
+
+
   return EXIT_SUCCESS;
 }
