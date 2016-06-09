f5317e4b771bc72023b8ff22138972741b06732a
Alex <alexandru.toma15@imperial.ac.uk>
2016-06-09 18:31:29 +0100

    Finished Part4

--- programs/main.s
+++ programs/main.s
@@ -1,15 +1,105 @@
-.section .init
-.global _start
-_start:
+ldr r0, =0x20200000
+ldr r1, =0x20200028
+ldr r2, =0x2020001C
+ldr r3, [r0]
+orr r3, r3, #0x1240
+str r3, [r0]
 
-ldr r0,=0x20200000
-mov r1,#1
-lsl r1,#18
-str r1,[r0,#4]
-
-mov r1,#1
-lsl r1,#16
-str r1,[r0,#40]
+mov r4, #4
+mov r5, #8
+mov r6, #16
 
 loop:
+
+mov r7, #4128768
+wait1:
+sub r7, r7, #1
+cmp r7, #0
+bne wait1
+
+str r4, [r1]
+str r5, [r1]
+str r6, [r1]
+
+str r6, [r2]
+
+mov r7, #4128768
+wait2:
+sub r7, r7, #1
+cmp r7, #0
+bne wait2
+
+str r4, [r1]
+str r5, [r1]
+str r6, [r1]
+
+str r5, [r2]
+
+mov r7, #4128768
+wait3:
+sub r7, r7, #1
+cmp r7, #0
+bne wait3
+
+str r4, [r1]
+str r5, [r1]
+str r6, [r1]
+
+str r5, [r2]
+str r6, [r2]
+
+mov r7, #4128768
+wait4:
+sub r7, r7, #1
+cmp r7, #0
+bne wait4
+
+str r4, [r1]
+str r5, [r1]
+str r6, [r1]
+
+str r4, [r2]
+
+mov r7, #4128768
+wait5:
+sub r7, r7, #1
+cmp r7, #0
+bne wait5
+
+str r4, [r1]
+str r5, [r1]
+str r6, [r1]
+
+str r4, [r2]
+str r6, [r2]
+
+mov r7, #4128768
+wait6:
+sub r7, r7, #1
+cmp r7, #0
+bne wait6
+
+str r4, [r1]
+str r5, [r1]
+str r6, [r1]
+
+str r4, [r2]
+str r5, [r2]
+
+mov r7, #4128768
+wait7:
+sub r7, r7, #1
+cmp r7, #0
+bne wait7
+
+str r4, [r1]
+str r5, [r1]
+str r6, [r1]
+
+str r4, [r2]
+str r5, [r2]
+str r6, [r2]
+
+
 b loop
+andeq r0, r0, r0
